f745dba4e57b4662f52e04c45cefbd8c88ca367c
LumSeg <lucas_lms_24@hotmail.com>
2025-06-07 19:14:34 -0300

    Cleanup: keep only backend code for frontend-dev branch

--- server/src/repositories/ProductRepository.ts
+++
@@ -1,5 +0,0 @@
-import { Repository } from "typeorm";
-import { AppDataSource } from "../config/dbConfig"; 
-import { Product } from "../entities/Product";
-
-export const ProductRepository: Repository<Product> = AppDataSource.getRepository(Product);
