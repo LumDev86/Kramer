a73e07ead4ee8ea23e68b86092d172a8b29081e5
juanRCoder <guillecarck@gmail.com>
2025-06-23 17:26:43 -0500

    feat: consume the API to obtain products by category

---
+++ web/src/services/getProductsByCategory.ts
@@ -0,0 +1,17 @@
+const apiUrl = import.meta.env.VITE_URI;
+
+export const getProductsByCategory = async (category: string) => {
+  try {
+    const response = await fetch(`${apiUrl}/api/categories/${category}/products`);
+
+    if (!response.ok) {
+      throw new Error(`Error ${response.status}: ${response.statusText}`);
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('[getProductsByCategory] Error fetching by data:', error);
+    throw error;
+  }
+};
