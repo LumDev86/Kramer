977bb826e54e770f836ddf32168c2f80490da265
LumSeg <lms.segovia86@gmail.com>
2025-06-25 17:20:02 -0300

    " "

--- web/src/contexts/CartContext.ts
+++
@@ -1,4 +0,0 @@
-import { createContext } from "react";
-import { CartContextType } from "../interfaces/cart";
-
-export const CartContext = createContext<CartContextType | undefined>(undefined);
--- web/src/interfaces/category.ts
+++
@@ -1,5 +0,0 @@
-export interface CategoryProps {
-  to: string;
-  label: string;
-  img: string;
-}
