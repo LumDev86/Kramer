8fdc955134ba2e3319b80d34013138247e2e6a07
juanRCoder <guillecarck@gmail.com>
2025-07-04 18:56:09 -0500

    feat: re-design product card

--- web/src/components/cards/Product.tsx
+++ web/src/components/cards/Product.tsx
@@ -2,6 +2,7 @@ import { ProductProps } from "../../interfaces/product";
 import { useCart } from "../../hooks/useCart";
 import { useAddProductToCart } from "../../hooks/useAddProductToCart";
 import { toast } from "sonner";
+import { ShoppingCart, Check } from "lucide-react";
 
 export const Product = ({ product }: ProductProps) => {
   const { addToCart } = useCart();
@@ -32,20 +33,20 @@ export const Product = ({ product }: ProductProps) => {
   };
 
   return (
-    <div className="bg-white rounded-lg shadow-md p-4">
-      <img src={product.image} alt={product.name} />
+    <div className="font-outfit gap-3 flex flex-col">
+      <div className="bg-[#6EC3F64D] h-[134px] relative rounded-lg cursor-pointer flex flex-col 
+      justify-center items-center font-outfit gap-3">
+        <img src={product.image} alt={product.name} />
+      </div>
       <h3 className="text-lg font-semibold">{product.name}</h3>
-      <p className="text-gray-600">{product.description}</p>
-      <p className="text-gray-700 font-bold">${product.price}</p>
-      <p className="text-gray-500">Marca: {product.brand}</p>
-      <p className="text-gray-500">Peso: {product.weight}</p>
+      <p className="text-2xl font-medium">${product.price}</p>
       <button
         onClick={handleAddToCart}
         disabled={isPending}
-        className={`mt-3 w-full px-4 py-1 rounded 
-          ${isPending ? "bg-gray-400" : "bg-green-500 hover:bg-green-600 text-white"}`}
+        className={`w-full flex items-center justify-center gap-2 text-lg rounded-full py-2 
+          ${isPending ? "bg-[#8de68aa3] text-[#242424a3]" : "bg-[#8de68a] hover:bg-green-60 text-[#242424]"}`}
       >
-        {isPending ? "Agregando..." : "Agregar al carrito"}
+        {isPending ? (<Check color="#242424a3" />) : (<ShoppingCart fill="#242424" />)} Agregar
       </button>
     </div>
   );
--- web/src/components/loaders/Product.tsx
+++ web/src/components/loaders/Product.tsx
@@ -1,14 +1,13 @@
 export const Product = () => {
   return (
     <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
-      <div className="bg-white rounded-lg shadow-md p-4 animate-pulse">
-        <div className="w-full h-5 bg-gray-300 rounded-md mb-2"></div>
-        <div className="h-7 bg-gray-300 rounded w-3/4 mb-2"></div>
-        <div className="h-4 bg-gray-300 rounded w-full mb-1"></div>
-        <div className="h-5 bg-gray-300 rounded w-1/2 mb-1"></div>
-        <div className="h-4 bg-gray-300 rounded w-1/3 mb-1"></div>
-        <div className="h-4 bg-gray-300 rounded w-1/4 mb-4"></div>
-        <div className="h-8 rounded bg-gray-400"></div>
+      <div className="font-outfit gap-3 flex flex-col animate-pulse">
+        <div className="bg-[#6EC3F64D] h-[134px] relative rounded-lg flex justify-center items-center">
+          <div className="w-20 h-20 bg-gray-300 rounded-md" />
+        </div>
+        <div className="h-5 bg-gray-300 rounded w-3/4" />
+        <div className="h-6 bg-gray-300 rounded w-1/2" />
+        <div className="h-10 rounded-full bg-gray-400 w-full" />
       </div>
     </div>
   );
