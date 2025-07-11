2a781089d4a0cb8353e41373e17f9bad5ed006b9
Maria Alarcon <mariagaa993@gmail.com>
2025-07-11 18:47:13 -0300

    cambiar number por string en la propiedad price

--- web/src/utils/mocks.ts
+++ web/src/utils/mocks.ts
@@ -10,10 +10,10 @@ export const bannerCarousel = [
 ];
 
 export const suggestions = [
-    { id: "1", name: "Prueba", price: 2.99, image: img3, description: "OK", brand: "OK", weight: "OK" },
-    { id: "2", name: "Example", price: 1.99, image: img3, description: "OK", brand: "OK", weight: "OK" },
-    { id: "3", name: "Surtido", price: 3.45, image: img3, description: "OK", brand: "OK", weight: "OK" },
-    { id: "4", name: "Prueba", price: 10.99, image: img3, description: "OK", brand: "OK", weight: "OK" },
-    { id: "5", name: "Example", price: 9.99, image: img3, description: "OK", brand: "OK", weight: "OK" },
-    { id: "6", name: "Surtido", price: 2.99, image: img3, description: "OK", brand: "OK", weight: "OK" },
+    { id: "1", name: "Prueba", price: "2.99", image: img3, description: "OK", brand: "OK", weight: "OK" },
+    { id: "2", name: "Example", price: "1.99", image: img3, description: "OK", brand: "OK", weight: "OK" },
+    { id: "3", name: "Surtido", price: "3.45", image: img3, description: "OK", brand: "OK", weight: "OK" },
+    { id: "4", name: "Prueba", price: "10.99", image: img3, description: "OK", brand: "OK", weight: "OK" },
+    { id: "5", name: "Example", price: "9.99", image: img3, description: "OK", brand: "OK", weight: "OK" },
+    { id: "6", name: "Surtido", price: "2.99", image: img3, description: "OK", brand: "OK", weight: "OK" },
 ];
