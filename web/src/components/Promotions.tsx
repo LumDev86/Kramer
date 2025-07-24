5753e64242a7fe5fc852ce57eebc25718298d44b
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-07-24 14:48:13 -0300

    feat: adds useState and useEffect to Promotions component to handle getAllPromotions function and removes suggestions array

--- web/src/components/Promotions.tsx
+++ web/src/components/Promotions.tsx
@@ -1,17 +1,50 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import { FreeMode } from "swiper/modules";
 import { Product } from "./cards/Product";
-// import { Product as Loader } from "./loaders/Product";
-import { suggestions } from "../utils/mocks";
+import { getAllPromotions } from "../services/getAllPromotions";
+import { useEffect, useState } from "react";
+import { Product as ProductProps } from "../interfaces/product";
+import { Category } from "../interfaces/category";
+
+export interface PromotionData {
+  percent?: number;
+  amount?: number;
+  freeShipping?: boolean;
+}
+
+export interface Promotion {
+  id: string;
+  title: string;
+  description: string;
+  type: string;
+  data: PromotionData;
+}
+
+export interface ProductWithDetails extends ProductProps {
+  imagePublicId?: string | null;
+  category?: Category;
+  promotion?: Promotion;
+}
 
 export const Promotions = () => {
-  // const { data = [], isLoading, error } = ();
+  const [promotions, setPromotions] = useState<ProductWithDetails[]>([]);
 
-  // if (isLoading) return <Loader />;
-  // if (error) return <p className="text-red-500">Error cargando la información</p>;
+  useEffect(() => {
+    const fetchPromotions = async () => {
+      try {
+        const data = await getAllPromotions();
+
+        setPromotions(data);
+      } catch (err) {
+        console.error("Error:", err);
+      }
+    };
+
+    fetchPromotions();
+  }, []);
 
   return (
-   <section>
+    <section>
       <h2 className="font-bold text-2xl pb-4">Promociones</h2>
       <Swiper
         slidesPerView={2.3}
@@ -35,15 +68,14 @@ export const Promotions = () => {
           1200: {
             slidesPerView: 6.5,
             spaceBetween: 25,
-          }
-        }}>
-        {
-          suggestions.map(product => (
-            <SwiperSlide key={product.id}>
-              <Product product={product} />
-            </SwiperSlide>
-          ))
-        }
+          },
+        }}
+      >
+        {promotions.map((product) => (
+          <SwiperSlide key={product.id}>
+            <Product product={product} />
+          </SwiperSlide>
+        ))}
       </Swiper>
     </section>
   );
