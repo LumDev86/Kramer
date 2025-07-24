142763430160f4467f57057cdb7d8b92988e5265
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-07-24 15:10:48 -0300

    refactor: imports usePromotions hook in Promotions component to separate logic from visual components

--- web/src/components/Promotions.tsx
+++ web/src/components/Promotions.tsx
@@ -1,47 +1,10 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import { FreeMode } from "swiper/modules";
 import { Product } from "./cards/Product";
-import { getAllPromotions } from "../services/getAllPromotions";
-import { useEffect, useState } from "react";
-import { Product as ProductProps } from "../interfaces/product";
-import { Category } from "../interfaces/category";
-
-export interface PromotionData {
-  percent?: number;
-  amount?: number;
-  freeShipping?: boolean;
-}
-
-export interface Promotion {
-  id: string;
-  title: string;
-  description: string;
-  type: string;
-  data: PromotionData;
-}
-
-export interface ProductWithDetails extends ProductProps {
-  imagePublicId?: string | null;
-  category?: Category;
-  promotion?: Promotion;
-}
+import { usePromotions } from "../hooks/usePromotions";
 
 export const Promotions = () => {
-  const [promotions, setPromotions] = useState<ProductWithDetails[]>([]);
-
-  useEffect(() => {
-    const fetchPromotions = async () => {
-      try {
-        const data = await getAllPromotions();
-
-        setPromotions(data);
-      } catch (err) {
-        console.error("Error:", err);
-      }
-    };
-
-    fetchPromotions();
-  }, []);
+  const { promotions } = usePromotions();
 
   return (
     <section>
