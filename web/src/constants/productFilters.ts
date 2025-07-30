c2a947994981632128bf7b91abcfac8cc1a7b380
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-07-30 12:41:16 -0300

    refactor: rename constants priceRangesTags to PRICE_RANGES_TAGS, priceRanges to PRICE_RANGES and sortOptions to SORT_OPTIONS in productFilter file in constants folder

--- web/src/constants/productFilters.ts
+++ web/src/constants/productFilters.ts
@@ -1,6 +1,6 @@
 import { PriceRangeId } from "../interfaces/product";
 
-export const priceRangesTags: {
+export const PRICE_RANGES_TAGS: {
   id: PriceRangeId;
   label: string;
   min: number;
@@ -11,13 +11,14 @@ export const priceRangesTags: {
   { id: "over100", label: ">$100", min: 101, max: Infinity },
 ];
 
-export const priceRanges: Record<PriceRangeId, { min: number; max: number }> = {
-  under50: { min: 0, max: 49 },
-  "between50-100": { min: 50, max: 100 },
-  over100: { min: 101, max: Infinity },
-};
+export const PRICE_RANGES: Record<PriceRangeId, { min: number; max: number }> =
+  {
+    under50: { min: 0, max: 49 },
+    "between50-100": { min: 50, max: 100 },
+    over100: { min: 101, max: Infinity },
+  };
 
-export const sortOptions = [
+export const SORT_OPTIONS = [
   { id: "bestsellers", label: "Más vendidos" },
   { id: "newest", label: "Nuevos productos" },
   { id: "promotions", label: "Promociones" },
