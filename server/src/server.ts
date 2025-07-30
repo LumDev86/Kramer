40e02335caf715f670099127bae26e0562dcb204
Jmjoss <josekandia42@gmail.com>
2025-07-30 15:15:13 -0300

    feat: add CORS

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
+import { config } from "@config/validateEnv";
 import router from "@routes/indexRoutes";
 import dotenv from "dotenv";
 import { setupSwagger } from "@config/swagger";
@@ -10,8 +11,25 @@ dotenv.config();
 const server = express();
 setupSwagger(server);
 
+
+
+const allowedOrigins = [config.HOST_SERVER_LOCAL, config.PORT_SERVER_PRODUCTION];
+
+const corsOptions: cors.CorsOptions = {
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("CORS policy does not allow this origin"));
+    }
+  },
+  credentials: true,
+};
+
 // Middlewares
-server.use(cors()); // Habilita CORS para todas las solicitudes
+server.use(cors(corsOptions));
 server.use(morgan("dev")); // Muestra logs de las peticiones en consola
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
