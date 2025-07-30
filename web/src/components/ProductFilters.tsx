b0c933510bddb38ad13a08faa7965e41735c97df
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-07-30 13:01:17 -0300

    refactor: imports useProductsFilters to ProductFilters.tsx component to improve code organization and reusability

--- web/src/components/ProductFilters.tsx
+++ web/src/components/ProductFilters.tsx
@@ -1,48 +1,8 @@
-import { FC, useState } from "react";
+import { FC } from "react";
 import { ChevronDown, Check } from "lucide-react";
-import { FilterState, PriceRangeId } from "../interfaces/product";
+import { ProductFiltersProps } from "../interfaces/product";
 import { PRICE_RANGES_TAGS, SORT_OPTIONS } from "../constants/productFilters";
-
-interface ProductFiltersProps {
-  filters: FilterState;
-  onFiltersChange: (filters: FilterState) => void;
-  resultsCount: number;
-}
-
-const useProductFilters = (
-  filters: FilterState,
-  onFiltersChange: (filters: FilterState) => void
-) => {
-  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-
-  const handlePriceRangeChange = (rangeId: PriceRangeId) => {
-    const newPriceRange = filters.priceRange === rangeId ? null : rangeId;
-    onFiltersChange({
-      ...filters,
-      priceRange: newPriceRange,
-    });
-  };
-
-  const handleSortChange = (sortId: string) => {
-    onFiltersChange({
-      ...filters,
-      sortBy: sortId,
-    });
-    setIsDropdownOpen(false);
-  };
-
-  const toggleDropdown = () => {
-    setIsDropdownOpen(!isDropdownOpen);
-  };
-
-  return {
-    isDropdownOpen,
-    setIsDropdownOpen,
-    handlePriceRangeChange,
-    handleSortChange,
-    toggleDropdown,
-  };
-};
+import { useProductFilters } from "../hooks/useProductFilters";
 
 export const ProductFilters: FC<ProductFiltersProps> = ({
   filters,
