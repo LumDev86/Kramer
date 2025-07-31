b079e9799a1a5cdd0b997789b1d8eeff4ad22349
LumSeg <lms.segovia86@gmail.com>
2025-07-31 13:37:43 -0300

    refactor: moved CORS configuration to a separate config file for better modularity

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
-import { config } from "@config/validateEnv";
+import { corsOptions } from "@config/corsConfig";
 import router from "@routes/indexRoutes";
 import dotenv from "dotenv";
 import { setupSwagger } from "@config/swagger";
@@ -11,28 +11,6 @@ dotenv.config();
 const server = express();
 setupSwagger(server);
 
-
-
-const allowedOrigins = [
-    config.HOST_SERVER_LOCAL,
-    config.PORT_SERVER_PRODUCTION,
-    config.HOST_PIXIAN,
-    config.HOST_FRONT_LOCAL,
-    config.HOST_VERCEL,
-];
-
-const corsOptions: cors.CorsOptions = {
-  origin: function (origin, callback) {
-    if (!origin) return callback(null, true);
-
-    if (allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("CORS policy does not allow this origin"));
-    }
-  },
-  credentials: true,
-};
 
 // Middlewares
 server.use(cors(corsOptions));
