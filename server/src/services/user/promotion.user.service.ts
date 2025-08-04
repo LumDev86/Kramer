5e6bfaaf04f76dbb71520035c1b7689e7d927d2a
LumSeg <lms.segovia86@gmail.com>
2025-08-04 15:15:26 -0300

    fix(promotion): implement pagination and include promotion type details with rules

--- server/src/services/user/promotion.user.service.ts
+++ server/src/services/user/promotion.user.service.ts
@@ -1,22 +1,33 @@
 import { PromotionRepository } from "@/repositories/PromotionRepository";
 
 export class PromotionService {
-    async getAll() {
-        const promotions = await PromotionRepository.find({
-        relations: ["products"],
-        order: { title: "ASC" },
+    async getAll(page = 1, limit = 10) {
+        const [promotions, total] = await PromotionRepository.findAndCount({
+            relations: ["products"],
+            order: { title: "ASC" },
+            skip: (page - 1) * limit,
+            take: limit,
         });
-        return promotions;
+
+        return {
+            pagination: {
+                total,
+                page,
+                limit,
+                totalPages: Math.ceil(total / limit),
+            },
+            promotions,
+        };
     }
 
     async getById(id: string) {
         const promotion = await PromotionRepository.findOne({
-        where: { id },
-        relations: ["products"],
+            where: { id },
+            relations: ["products"],
         });
-    
+
         if (!promotion) throw new Error("Promoción no encontrada.");
-    
+
         return promotion;
     }
 }
